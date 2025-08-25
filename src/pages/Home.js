dd084a2ad1ac202e7114e54a689433d06bbfe9e0
rosa1315 <rosayepes03@gmail.com>
2025-08-24 21:21:49 -0500

    cartas

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import API from '../services/api';
 import { Link } from 'react-router-dom';
-
+import "../components/login.css";
 
 function Home() {
   const [posts, setPosts] = useState([]);
@@ -13,7 +13,7 @@ function Home() {
   }, []);
 
   return (
-    <div className="admin-panel container-fluid text-white">
+    <div className="letra admin-panel container-fluid text-white">
       <div className="col-md-12 text-center mb-5">
         <div className="admin text-center">
           <h1 className=" display-4">¡Bienvenidos a mi Blog!</h1>
@@ -21,26 +21,26 @@ function Home() {
           <p className="lead">Los invito a leer el contenido</p>
         </div>
       <hr className="my-5" />
-      <div className="row">
+      <div className="row ">
         <div className="col-md-4">
-          <div className=" card shadow-sm">
-            <div className="home card-body">
+          <div className="shadow-sm">
+            <div className="home card-body text-black">
               <h5 className="card-title" ><a className="nav-link active" aria-current="page" href="/admin" >Temas del Blog</a></h5>
               <p className="card-text">Educación, tecnología y proyectos personales.</p>
             </div>
           </div>
         </div>
         <div className="col-md-4">
-          <div className="card shadow-sm">
-            <div className="home card-body">
+          <div className="shadow-sm">
+            <div className="home card-body text-black ">
               <h5 className="card-title" ><a className="nav-link active" aria-current="page" href="/sobremi" >Sobre Mi</a></h5>
               <p className="card-text">Soy docente y tecnóloga en desarrollo de software. Este blog une ambas pasiones.</p>
             </div>
           </div>
         </div>
         <div className="col-md-4">
-          <div className="card shadow-sm">
-            <div className="home card-body">
+          <div className="shadow-sm">
+            <div className="home card-body text-black">
               <h5 className="card-title" ><a className="nav-link active" aria-current="page" href="/conctacto" >¿Quieres contactarme?</a></h5>
               <p className="card-text">Me podras escribir a mi correo...</p>
             </div>
